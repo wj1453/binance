22fb4da65838e691399b84d5abaed0bfce8e7de5
wj1453 <286521235@qq.com>
2017-12-03 23:06:30 +0800

    123123

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <mt-header :title="pageTitle">
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data:function(){
+    return {
+      'pageTitle':'首页'
+    }
+  }
+}
+</script>
+
+<style>
+body{margin:0;padding:0;}
+</style>
